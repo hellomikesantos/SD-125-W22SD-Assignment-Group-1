e07b81bc7d61f8140c1dd500310a67293aec998a
Dane Dwarka <danedwarka@hotmail.com>
2022-09-12 12:58:25 -0500

    Add project files.

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
