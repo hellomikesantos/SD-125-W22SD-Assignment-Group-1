7ce3eed1c41e541b312e9c8a8a66c28411506982
Dane Dwarka <danedwarka@hotmail.com>
2022-09-12 21:42:40 -0500

    Create Project controller and views

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SD_340_W22SD_2021_2022___Final_Project_2.Data;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+
+            return View();
+        }
+    }
+}
