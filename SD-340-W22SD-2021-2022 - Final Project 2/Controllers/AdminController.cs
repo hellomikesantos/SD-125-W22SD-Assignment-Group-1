fdc1b93529524331c3f20cf27c5b2121512096e7
Jaesung Hong <jezsung@gmail.com>
2022-09-17 11:35:25 +0900

    Add Assigning Developer

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SD_340_W22SD_2021_2022___Final_Project_2.Data;
+using SD_340_W22SD_2021_2022___Final_Project_2.Models;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UnassignedDevelopers()
+        {
+            List<string> userIds = _context.UserRoles.Select(ur => ur.UserId).ToList();
+            List<ApplicationUser> users = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignDeveloper(string userId)
+        {
+            ApplicationUser? user = _context.Users.Find(userId);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Developer");
+
+            return RedirectToAction(nameof(UnassignedDevelopers));
+        }
+
+       
+    }
+}
