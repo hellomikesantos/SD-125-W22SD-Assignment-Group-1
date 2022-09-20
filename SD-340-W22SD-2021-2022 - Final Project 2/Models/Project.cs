126e5d6c9dd76efc832fb7b23b341d80eb3d0468
Dane Dwarka <danedwarka@hotmail.com>
2022-09-19 19:12:32 -0500

    Make Project.Name maxlength = 200

--- SD-340-W22SD-2021-2022 - Final Project 2/Models/Project.cs
+++ SD-340-W22SD-2021-2022 - Final Project 2/Models/Project.cs
@@ -10,7 +10,7 @@ namespace SD_340_W22SD_2021_2022___Final_Project_2.Models
 
         [Required(ErrorMessage = "Name is required.")]
         [MinLength(5, ErrorMessage = "Name must be at least 5 characters long.")]
-        [MaxLength(20, ErrorMessage = "Name must be less than or equal to 200 characters.")]
+        [MaxLength(200, ErrorMessage = "Name must be less than or equal to 200 characters.")]
         public string Name { get; set; }
         public virtual ICollection<Ticket> Ticket { get; set; } = new HashSet<Ticket>();
 
