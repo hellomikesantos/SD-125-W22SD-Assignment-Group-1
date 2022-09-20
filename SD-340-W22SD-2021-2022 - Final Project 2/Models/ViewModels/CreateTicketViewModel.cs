e2bcc274941cebfd8fa2a2c9557d522788d4c842
Dane Dwarka <danedwarka@hotmail.com>
2022-09-19 20:44:39 -0500

    Create CreateTicketViewModel

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Models/ViewModels/CreateTicketViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SD_340_W22SD_2021_2022___Final_Project_2.Models.ViewModels
+{
+    public class CreateTicketViewModel
+    {
+        public int ProjectId { get; set; }
+        public Ticket Ticket { get; set; }
+        public List<ApplicationUser> Developers { get; set; } = new List<ApplicationUser>();
+
+        public CreateTicketViewModel(int projectId, Ticket ticket, List<ApplicationUser> developers)
+        {
+            ProjectId = projectId;
+            Ticket = ticket;
+            Developers = developers;
+        }
+    }
+}
