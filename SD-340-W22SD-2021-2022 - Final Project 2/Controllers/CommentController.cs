7229aa8fd15b33b321331b1761bcbd857ce5a9b5
Dane Dwarka <danedwarka@hotmail.com>
2022-09-20 19:43:40 -0500

    Add a view to get comments by TicketId

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/CommentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SD_340_W22SD_2021_2022___Final_Project_2.Data;
+using SD_340_W22SD_2021_2022___Final_Project_2.Models;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
+{
+    public class CommentController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> CommentsForTask(int ticketId)
+        {
+            List<Comment> comments;
+
+            try
+            {
+                comments = _context.Comment
+                    .Where(c => c.TicketId == ticketId)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Project");
+            }
+
+            return View();
+        }
+    }
+}
