114df6fc89359888dea6a79e142c8ae58b098a1b
Frederick Lopez <mtester010@gmail.com>
2022-10-07 17:05:26 -0500

    Added IRepository class and folder structure for DAL and BLL

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/DAL/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace SD_340_W22SD_2021_2022___Final_Project_2.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        //Add
+        void Add(T entity);
+        //Read
+        T Get(int id);
+        T Get(Func<T, bool> predicate);
+        ICollection<T> GetAll();
+        ICollection<T> GetList(Func<T, bool> predicate);
+        //Update
+        T Update(T entity);
+        //Delete
+        void Delete(T entity);
+        //Save
+        void Save();
+    }
+}
