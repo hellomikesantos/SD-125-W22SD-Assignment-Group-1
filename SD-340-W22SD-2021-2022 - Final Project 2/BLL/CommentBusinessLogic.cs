70db33826810ac36ab49b14c9d24c71eef65363b
Fulin <944799613@qq.com>
2022-10-07 22:44:39 -0500

    add comment repository & BL

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/BLL/CommentBusinessLogic.cs
@@ -0,0 +1,7 @@
+﻿namespace SD_340_W22SD_2021_2022___Final_Project_2.BLL
+{
+    public class CommentBusinessLogic
+    {
+
+    }
+}
---
+++ SD-340-W22SD-2021-2022 - Final Project 2/DAL/CommentRepository.cs
@@ -0,0 +1,47 @@
+﻿using SD_340_W22SD_2021_2022___Final_Project_2.Models;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.DAL
+{
+    public class CommentRepository : IRepository<Comment>
+    {
+        public void Add(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Get(Func<Comment, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Comment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Comment> GetList(Func<Comment, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Update(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
