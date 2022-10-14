445fa643416d17795c306ce5d34cca0b48009626
Frederick Lopez <mtester010@gmail.com>
2022-10-14 15:18:31 -0500

    Added try catch in project business logic get all project by developer

--- SD-340-W22SD-2021-2022 - Final Project 2/BLL/ProjectBusinessLogic.cs
+++ SD-340-W22SD-2021-2022 - Final Project 2/BLL/ProjectBusinessLogic.cs
@@ -14,23 +14,46 @@ namespace SD_340_W22SD_2021_2022___Final_Project_2.BLL
             this.repo = repo;
             this._userManager = userManager;
         }
-
+        // To UnitTest
+        // Valid: Test if project list is returning the correct count
+        // Invalid: Test if count is 0 if invalid devId
         public async Task<List<Project>> GetAllProjectsByDeveloperAsync(string devId)
         {
-            ApplicationUser currUser = await _userManager.FindByIdAsync(devId);
-            return repo.GetList(project => project.Developers.Contains(currUser)).ToList();
-        }
+            try
+            {
+                ApplicationUser currUser = await _userManager.FindByIdAsync(devId);
+                return repo.GetList(project => project.Developers.Contains(currUser)).ToList();
+            } catch
+            {
+                throw new NullReferenceException("No Developer found with the given ID");
+            }
 
+        }
+        // To UnitTest
+        // Valid: Test if project list is returning the correct count.
+        // Invalid:
         public List<Project> GetAllProjects()
         {
             return repo.GetAll().ToList();
         }
-
+        // To UnitTest
+        // Valid: Test if the correct project is being returned
+        // Invalid: Test if the throws an exception if project not found.
         public Project GetProjectDetails(int projId)
         {
-            return repo.Get(projId);
+            try
+            {
+                Project currProject = repo.Get(projId);
+                return currProject;
+            } catch
+            {
+                throw new NullReferenceException("Project not found");
+            }
+            
         }
-
+        // To UnitTest
+        // Valid: Test if Project count is incrementing
+        // Invalid: If ticket count is still the same
         public void CreateProject(Project project)
         {
             repo.Create(project);
